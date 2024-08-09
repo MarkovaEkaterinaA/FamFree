927d3932189e89cbb6115561c288de4d8a463eb5
MarkovaEkaterinaA <159148652+MarkovaEkaterinaA@users.noreply.github.com>
2024-08-09 12:38:14 +0300

    Delete Project JAVA/FamilyTreeProject/src/com/project directory

--- Project JAVA/FamilyTreeProject/src/com/project/familytree/model/Gender.java
+++
@@ -1,7 +0,0 @@
-package com.project.familytree.model;
-
-import java.io.Serializable;
-
-public enum Gender implements Serializable {
-    MALE, FEMALE
-}
--- Project JAVA/FamilyTreeProject/src/com/project/familytree/view/FamilyTreeView.java
+++
@@ -1,9 +0,0 @@
-package com.project.familytree.view;
-
-import com.project.familytree.model.Human;
-import java.util.List;
-
-public interface FamilyTreeView {
-    void showFamilyTree(List<Human> members);
-    void showError(String message);
-}
