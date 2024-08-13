e753b883908bed503c22bcc7a36c8b78299fb8cb
MarkovaEkaterinaA <159148652+MarkovaEkaterinaA@users.noreply.github.com>
2024-08-13 16:36:58 +0300

    Delete Project JAVA/FamilyTreeProject directory

--- Project JAVA/FamilyTreeProject/src/com/project/familytree/data/IDataHandler.java
+++
@@ -1,8 +0,0 @@
-package com.project.familytree.data;
-
-import java.io.IOException;
-
-public interface IDataHandler<T> {
-    void save(T data, String filename) throws IOException;
-    T load(String filename) throws IOException, ClassNotFoundException;
-}
--- Project JAVA/FamilyTreeProject/src/com/project/familytree/model/IFamilyTree.java
+++
@@ -1,9 +0,0 @@
-package com.project.familytree.model;
-
-import java.util.List;
-
-public interface IFamilyTree<T extends Human> {
-    void addMember(T human);
-    T findByName(String name);
-    List<T> getMembers();
-}
